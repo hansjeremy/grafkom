8f9606708d8cac1fd37fc25985560d8160dbfd57
KOMP_SI\INFOR <estjk>
2023-02-13 10:21:08 +0700

    minggu 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+import Engine.Window;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL30.*;
+
+public class Main {
+
+    private Window window =
+            new Window(800,800,"Hello World");
+    public void init(){
+        window.init();
+        GL.createCapabilities();
+
+
+    }
+
+    public void loop(){
+        while (window.isOpen()){
+            window.update();
+            glClearColor(0.0f,0.0f,0.0f,0.0f);
+            GL.createCapabilities();
+
+            //code
+
+
+            glDisableVertexAttribArray(0);
+            glfwPollEvents();
+        }
+    }
+
+    public void run(){
+        init();
+        loop();
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+    public static void main(String[] args) {
+        new Main().run();
+    }
+}
